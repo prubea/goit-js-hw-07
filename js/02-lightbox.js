b48afbca4daca2859b0c0fb46fe94d40f82beac8
Beata Pruszczyk <prubea@gmail.com>
2022-07-24 20:43:42 +0200

    02-lightbox
    Zadanie 2
    wykorzystanie z biblioteki SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector("ul.gallery");
+
+const markup = galleryItems
+    .map(elem => {
+      const images = `
+  <a class="gallery__link" href="${elem.original}">
+    <img
+      class="gallery__image"
+      src="${elem.preview}"
+      title="${elem.description}"
+      alt="${elem.description}"
+    />
+  </a>`;
+    return images;
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+new SimpleLightbox('.gallery a' ,{ captionDelay: 250 });
+
+
 console.log(galleryItems);
